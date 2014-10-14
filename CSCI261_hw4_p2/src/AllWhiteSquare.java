f25f5b324e131c5dc5db17d0834322f5d75b109c
Derek Brown <djb3718@rit.edu>
2014-10-14 14:20:42 -0400

    Added to eclipse and created java file

---
+++ CSCI261_hw4_p2/src/AllWhiteSquare.java
@@ -0,0 +1,15 @@
+/**
+ * AllWhiteSquare.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose	Dynamic programming approach for finding the largest all white k x k
+ * 			square grid, given an n x n grid with cells colored black or white
+ */
+
+public class AllWhiteSquare {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end AllWhiteSpace
